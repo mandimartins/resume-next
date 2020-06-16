bfd27418580830b1610f85cd7a53fc0f254b64e9
mandimartins <mandimartins23@gmail.com>
2020-06-16 17:33:48 -0300

    Filter irrelevant repos

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const index = (props) => {
       <div>{props.currentDate}</div>
       {props.repos.map((repo) => {
         return (
-          <div>
+          <div key={repo.id}>
             <h3>{repo.full_name}</h3>
             <p>
               Language: {repo.language} / Stars: {repo.stargazers_count}
@@ -20,15 +20,38 @@ const index = (props) => {
 };
 
 export async function getServerSideProps(context) {
-  const res = fetch(
+  const res = await fetch(
     'https://api.github.com/users/mandimartins/repos?sort=updated'
   );
-  const repos = await (await res).json();
+  const repos = await res.json();
+
+  const excludedRepos = [
+    'mandimartins/js-tdd',
+    'mandimartins/gerar-pdf',
+    'mandimartins/react-hooks',
+    'mandimartins/s3Upload',
+    'mandimartins/CI-CD-teste',
+  ];
+
+  const isNotFork = (repo) => !repo.fork;
+  const allowedRepos = (repo) => !excludedRepos.includes(repo.full_name);
+
+  const extractData = (repo) => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    stargazers_count: repo.stargazers_count,
+  });
+
+  const filteredRepos = repos
+    .filter(isNotFork)
+    .filter(allowedRepos)
+    .map(extractData);
 
   return {
     props: {
       currentData: new Date().toString(),
-      repos,
+      repos: filteredRepos,
     },
   };
 }
