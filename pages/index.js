b83b1eaf376533d050f2169bbbb84fadcd83a453
mandimartins <mandimartins23@gmail.com>
2020-06-16 16:37:36 -0300

    Get repos from github

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
-const index = () => {
+const index = (props) => {
   return (
     <div>
       <h1>Bem-vindo!</h1>
+      <div>{props.currentDate}</div>
+      {props.repos.map((repo) => {
+        return (
+          <div>
+            <h3>{repo.full_name}</h3>
+            <p>
+              Language: {repo.language} / Stars: {repo.stargazers_count}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  const res = fetch(
+    'https://api.github.com/users/mandimartins/repos?sort=updated'
+  );
+  const repos = await (await res).json();
+
+  return {
+    props: {
+      currentData: new Date().toString(),
+      repos,
+    },
+  };
+}
 
 export default index;
