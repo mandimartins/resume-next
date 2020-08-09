853104db6bcb7cc97a69fd5d98d7fdade1d1c12f
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-09 19:39:44 -0300

    Change google font ref to Titilium

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Titillium+Web:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
